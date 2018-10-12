759f5f38f86b92e560f5b8f569fd4bff0fdf58a4
kk412027247 <kk412027247@gmail.com>
2018-10-12 23:14:07 +0800

    init

---
+++ app/src/main/java/com/tmd/openanotherapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.tmd.openanotherapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent action = new Intent(Intent.ACTION_VIEW);
+                action.setData(Uri.parse("peopleapp://people/1"));
+                startActivity(action);
+            }
+        });
+    }
+}
